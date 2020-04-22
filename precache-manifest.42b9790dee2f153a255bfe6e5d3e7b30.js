bfd6a20db9daea1184d182c5e512437d59bc5e1c
Aro Matevosyan <AroMatevosyan777@gmil.com>
2020-04-22 20:04:54 +0400

    Updates

---
+++ precache-manifest.42b9790dee2f153a255bfe6e5d3e7b30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5e0c112648f89a753404f0506b7611e",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "f3d9e0301acfacee2172",
+    "url": "/todo-list/static/css/main.884088ac.chunk.css"
+  },
+  {
+    "revision": "b3882bc4770bdf234627",
+    "url": "/todo-list/static/js/2.bebd0e3c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-list/static/js/2.bebd0e3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3d9e0301acfacee2172",
+    "url": "/todo-list/static/js/main.0e683ed5.chunk.js"
+  },
+  {
+    "revision": "14abea8699182d33e5f7",
+    "url": "/todo-list/static/js/runtime-main.425acb26.js"
+  }
+]);
